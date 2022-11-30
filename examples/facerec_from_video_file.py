0610dc413b576c72624d650af3856c2786ed697b
Mingeon Choi <markhappy@naver.com>
2022-11-30 20:40:13 +0900

    Update facerec_from_video_file.py

--- examples/facerec_from_video_file.py
+++ examples/facerec_from_video_file.py
@@ -8,23 +8,26 @@ import cv2
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Open the input movie file
-input_movie = cv2.VideoCapture("hamilton_clip.mp4")
+input_movie = cv2.VideoCapture("C:/Users/Mingeon_Choi/face_recognition/examples/example.mp4")
 length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+# width = input_movie.get(cv2.CAP_PROP_FRAME_WIDTH)
+# print(width)
+# height = input_movie.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 # Create an output movie file (make sure resolution/frame rate matches input video!)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_movie = cv2.VideoWriter('output.avi', fourcc, 29.97, (640, 360))
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+output_movie = cv2.VideoWriter('C:/Users/Mingeon_Choi/face_recognition/examples/output2.avi', fourcc, 29.97, (1920, 1080))
 
 # Load some sample pictures and learn how to recognize them.
-lmm_image = face_recognition.load_image_file("lin-manuel-miranda.png")
-lmm_face_encoding = face_recognition.face_encodings(lmm_image)[0]
+jisoo_image = face_recognition.load_image_file("C:/Users/Mingeon_Choi/face_recognition/examples/jisoo.jpg")
+jisoo_face_encoding = face_recognition.face_encodings(jisoo_image)[0]
 
-al_image = face_recognition.load_image_file("alex-lacamoire.png")
-al_face_encoding = face_recognition.face_encodings(al_image)[0]
+rose_image = face_recognition.load_image_file("C:/Users/Mingeon_Choi/face_recognition/examples/rose.jpg")
+rose_face_encoding = face_recognition.face_encodings(rose_image)[0]
 
 known_faces = [
-    lmm_face_encoding,
-    al_face_encoding
+    jisoo_face_encoding,
+    rose_face_encoding
 ]
 
 # Initialize some variables
@@ -38,35 +41,45 @@ while True:
     ret, frame = input_movie.read()
     frame_number += 1
 
+
     # Quit when the input video file ends
     if not ret:
+        print("break")
         break
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
     # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
 
     face_names = []
+    flag= True
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.43)
 
         # If you had more than 2 faces, you could make this logic a lot prettier
         # but I kept it simple for the demo
         name = None
+
         if match[0]:
-            name = "Lin-Manuel Miranda"
+            print("jisoo다")
+            name = "jisoo"
         elif match[1]:
-            name = "Alex Lacamoire"
+            print(" 와 로제다")
+            name = "rose"
+        else:
+            flag = False
+
 
         face_names.append(name)
 
     # Label the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         if not name:
+
             continue
 
         # Draw a box around the face
@@ -79,7 +92,10 @@ while True:
 
     # Write the resulting image to the output video file
     print("Writing frame {} / {}".format(frame_number, length))
-    output_movie.write(frame)
+    if flag:
+        output_movie.write(frame)
+    else:
+        continue
 
 # All done!
 input_movie.release()
